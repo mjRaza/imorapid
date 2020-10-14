3f7d925968649c4618cb35a17edf63021b6d47e4
Muhammad Jaffar Raza <44323917+mjRaza@users.noreply.github.com>
2020-10-14 14:29:22 +0500

    done

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+
+  const url = 'https://imorapidtransfer.com/login';
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+        source={{
+          uri: url,
+        }}
+        startInLoadingState
+        scalesPageToFit
+        javaScriptEnabled
+        style={{ flex: 1,marginTop:'8%' }}
+      />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
